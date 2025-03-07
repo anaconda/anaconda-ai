60d2aa02e4457036573807cfc378173a035d0389
Albert DeFusco <adefusco@anaconda.com>
2025-03-07 15:47:03 -0500

    llm: embed, matching, stopping

--- src/anaconda_models/llm.py
+++ src/anaconda_models/llm.py
@@ -12,10 +12,9 @@ from llm import hookimpl
 from llm.default_plugins.openai_models import Chat
 from llm.default_plugins.openai_models import OpenAIEmbeddingModel
 from rich.console import Console
-from rich.status import Status
 
 from anaconda_models.clients import get_default_client
-from anaconda_models.clients.base import ModelQuantization, Server
+from anaconda_models.clients.base import ModelQuantization, Server, LoadParams
 
 client = get_default_client()
 console = Console(stderr=True)
@@ -26,23 +25,15 @@ class AnacondaModelMixin:
     anaconda_model: ModelQuantization | None = None
     server: Server | None = None
 
-    def _create_and_start(self, embedding: bool = False) -> None:
+    def _create_and_start(self, embedding: bool | None = None) -> None:
         if self.server is None:
             model_name = self.model_id.split(":", maxsplit=1)[1]
-            text = f"{model_name} (creating)"
-            with Status(text, console=console) as display:
-                client = get_default_client()
-                self.server = client.servers.create(model_name)
-                status = client.servers.start(self.server)
-                text = f"{model_name} ({status.status})"
-                display.update(text)
-
-                while status.status != "running":
-                    status = client.servers.start(self.server)
-                    text = f"{model_name} ({status.status})"
-                    display.update(text)
-            console.print(f"[bold green]✓[/] {text}", highlight=False)
-            atexit.register(client.servers.stop, self.server)
+            self.server = client.servers.create(
+                model_name, load_params=LoadParams(embedding=embedding)
+            )
+            self.server.start(show_progress=True, console=console)
+            if not self.server._matched:
+                atexit.register(self.server.stop, console=console)
 
         self.api_base = self.server.openai_url
 
@@ -59,7 +50,7 @@ class AnacondaQuantizedChat(Chat, AnacondaModelMixin):
         )
 
     def execute(self, prompt, stream, response, conversation=None, key=None):  # type: ignore
-        self._create_and_start(embedding=False)
+        self._create_and_start()
         return super().execute(prompt, stream, response, conversation, key)
 
     def __str__(self) -> str:
@@ -87,21 +78,6 @@ class AnacondaQuantizedEmbedding(OpenAIEmbeddingModel, AnacondaModelMixin):
         return ([float(r) for r in result.embedding] for result in results)
 
 
-def _accepted_model_name_variants(
-    model_id: str, model_name: str, method: str, format: str
-) -> List[str]:
-    variants = []
-
-    for id_ in model_id, model_id.lower(), model_name, model_name.lower():
-        for method_ in method.upper(), method.lower():
-            for format_ in format.upper(), format.lower():
-                for sep in "_", "/":
-                    v = f"{id_}{sep}{method_}.{format_}"
-                    variants.append(v)
-
-    return variants
-
-
 @llm.hookimpl
 def register_models(register: Callable) -> None:
     for model in client.models.list():
@@ -120,16 +96,13 @@ def register_models(register: Callable) -> None:
 @hookimpl
 def register_embedding_models(register: Callable) -> None:
     for model in client.models.list():
-        if model.trainedFor != "sentence-similarity":
+        if model.metadata.trainedFor != "sentence-similarity":
             continue
-        for quant in sorted(model.quantizedFiles, key=lambda q: q.quantMethod):
-            method = quant.quantMethod
-            format = quant.format
+        for quant in model.metadata.quantizations:
+            if not quant.isDownloaded:
+                continue
 
-            # file_ids = _accepted_model_name_variants(
-            #     model_id, model_name, method, format
-            # )
-            file_ids = [f"{model.name}_{method}.{format.lower()}"]
-            for file_id in file_ids:
-                embed = AnacondaQuantizedEmbedding(model_id=f"anaconda:{file_id}")
-                register(embed)
+            quant_chat = AnacondaQuantizedEmbedding(
+                model_id=f"anaconda:{quant.modelFileName}"
+            )
+            register(quant_chat)
