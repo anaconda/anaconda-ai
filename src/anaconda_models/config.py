97d6c52b1c9f7f940cef8685278cf166cc7f30fb
Albert DeFusco <adefusco@anaconda.com>
2025-03-13 17:10:27 -0400

    drop kurator

--- src/anaconda_models/config.py
+++ src/anaconda_models/config.py
@@ -1,15 +1,10 @@
-import os
 import json
 from pathlib import Path
 from typing import Any
-from typing import Dict
 from typing import Literal
-from typing import Optional
-from typing import Union
 
 import platformdirs
 from pydantic import BaseModel
-from pydantic import field_validator
 
 from anaconda_cli_base.config import AnacondaBaseSettings
 from anaconda_models.exceptions import APIKeyMissing
@@ -41,29 +36,11 @@ class AINavigatorConfig(BaseModel):
         return key
 
 
-class KuratorConfig(BaseModel):
-    domain: str = "kurator.anaconda.com"
-    ssl_verify: bool = True
-    extra_headers: Optional[Union[Dict[str, str], str]] = None
-    run_on: Literal["local"] = "local"
-    local_servers_path: Path = Path("~/.ai-navigator/local-servers").expanduser()
-    models_path: Path = Path("~/.ai-navigator/models").expanduser()
-
-    @field_validator("local_servers_path")
-    def expand_variables_servers_path(cls, v: Union[Path, str]) -> Path:
-        return Path(os.path.expandvars(v)).expanduser()
-
-    @field_validator("models_path")
-    def expand_variables_models_path(cls, v: Union[Path, str]) -> Path:
-        return Path(os.path.expandvars(v)).expanduser()
-
-
 class Backends(BaseModel):
     ai_navigator: AINavigatorConfig = AINavigatorConfig()
-    kurator: KuratorConfig = KuratorConfig()
 
 
 class AnacondaModelsConfig(AnacondaBaseSettings, plugin_name="models"):
-    default_backend: Literal["kurator", "ai-navigator"] = "ai-navigator"
     backends: Backends = Backends()
+    default_backend: Literal["ai-navigator"] = "ai-navigator"
     stop_server_on_exit: bool = False
