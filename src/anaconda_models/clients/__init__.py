f7de2a901301e9de1cd43cad205ac129cbf16631
Albert DeFusco <adefusco@anaconda.com>
2025-03-14 15:11:16 -0400

    check default_backend config

--- src/anaconda_models/clients/__init__.py
+++ src/anaconda_models/clients/__init__.py
@@ -1,8 +1,14 @@
+from ..config import AnacondaModelsConfig
 from .ai_navigator import AINavigatorClient
+from .base import GenericClient
 
 
-def get_default_client(*args, **kwargs):
-    return AINavigatorClient(*args, **kwargs)
+def get_default_client(*args, **kwargs) -> GenericClient:
+    config = AnacondaModelsConfig()
+    if config.default_backend == "ai-navigator":
+        return AINavigatorClient(*args, **kwargs)
+    else:
+        raise ValueError(f"{config.default_backend} is not supported")
 
 
 __all__ = ["AINavigatorClient", "get_default_client"]
