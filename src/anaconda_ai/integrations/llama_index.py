ecb3aaa9cc7cd5c68aad6227b048d533d7dac611
Albert DeFusco <adefusco@anaconda.com>
2025-10-02 14:16:37 -0400

    llama-index embedding

--- src/anaconda_ai/integrations/llama_index.py
+++ src/anaconda_ai/integrations/llama_index.py
@@ -5,7 +5,9 @@ from typing import Union
 
 from llama_index.core.constants import DEFAULT_TEMPERATURE, DEFAULT_CONTEXT_WINDOW
 from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.base.llms.types import LLMMetadata
+from llama_index.core.callbacks.base import CallbackManager
 from pydantic import Field
 
 from ..clients import get_default_client
@@ -84,3 +86,64 @@ class AnacondaModel(OpenAI):
             model_name=self.model,
             server_config=server_config,
         )
+
+
+class AnacondaEmbeddingModel(OpenAIEmbedding):
+    _server_config: ServerConfig
+
+    def __init__(
+        self,
+        model_name: str,
+        embed_batch_size: int = 10,
+        dimensions: Optional[int] = None,
+        additional_kwargs: Optional[Dict[str, Any]] = None,
+        max_retries: int = 10,
+        timeout: float = 60.0,
+        reuse_client: bool = True,
+        callback_manager: Optional[CallbackManager] = None,
+        num_workers: Optional[int] = None,
+        api_params: Optional[Union[Dict[str, Any], APIParams]] = None,
+        load_params: Optional[Union[Dict[str, Any], LoadParams]] = None,
+        infer_params: Optional[Union[Dict[str, Any], InferParams]] = None,
+        **kwargs: Any,
+    ) -> None:
+        # ensure model is not passed in kwargs, will cause error in parent class
+        if "model" in kwargs:
+            raise ValueError(
+                "Use `model_name` instead of `model` to initialize OpenAILikeEmbedding"
+            )
+
+        client = get_default_client()
+
+        if load_params is None:
+            load_params = {"embedding": True}
+        elif isinstance(load_params, LoadParams):
+            load_params.embedding = True
+        else:
+            load_params["embedding"] = True
+
+        server = client.servers.create(
+            model_name,
+            api_params=api_params,
+            load_params=load_params,
+            infer_params=infer_params,
+        )
+        server.start()
+
+        super().__init__(
+            model_name=model_name,
+            embed_batch_size=embed_batch_size,
+            dimensions=dimensions,
+            callback_manager=callback_manager,
+            additional_kwargs=additional_kwargs,
+            api_key=server.api_key,
+            api_base=server.openai_url,
+            api_version="empty",
+            max_retries=max_retries,
+            reuse_client=reuse_client,
+            timeout=timeout,
+            num_workers=num_workers,
+            **kwargs,
+        )
+
+        self._server_config = server.serverConfig
