b212a2a033d72847b9b4a648e778c3fa1557b25f
izaheen@anaconda.com <izaheen@192.168.1.19>
2026-03-18 16:05:36 +0500

    test (cli) automate anaconda-ai install, configuration, login and help command

--- tests/e2e/specs/cli/anaconda-ai.spec.ts
+++ tests/e2e/specs/cli/anaconda-ai.spec.ts
@@ -1,10 +1,17 @@
 import { test } from '@fixture';
 
 test.describe('Anaconda AI CLI Commands @anaconda-ai', () => {
-  // Test to verify that all models are listed correctly
-  test('Install Anaconda AI package', async ({ anacondaAiCli }) => {
-    const result = await anacondaAiCli.runInstallAiPackageCommand();
-    anacondaAiCli.verifyInstallAiPackageCommand(result);
+  test('anaconda ai --help', async ({ anacondaAiCli }) => {
+    const result = await anacondaAiCli.runAnacondaAiHelpCommand();
+    anacondaAiCli.verifyAnacondaAiHelpCommand(result);
+  });
+  test('anaconda ai --help1', async ({ anacondaAiCli }) => {
+    const result = await anacondaAiCli.runAnacondaAiHelpCommand();
+    anacondaAiCli.verifyAnacondaAiHelpCommand(result);
+  });
+  test('anaconda ai --help2', async ({ anacondaAiCli }) => {
+    const result = await anacondaAiCli.runAnacondaAiHelpCommand();
+    anacondaAiCli.verifyAnacondaAiHelpCommand(result);
   });
 
 });
--- tests/test-setup/global-setup.ts
+++ tests/test-setup/global-setup.ts
@@ -1,8 +1,29 @@
 /**
  * global-setup.ts: Sets up global state before all tests start.
- * Add any initialization setup code within this function.
+ * Runs the Anaconda AI package install before any tests.
  */
 
-export default (): void => {
-  // Add any initialization setup here
+import { AnacondaAiCli } from '../e2e/pages/cli/anaconda-ai';
+
+export default async (): Promise<void> => {
+  const anacondaAiCli = new AnacondaAiCli();
+  // Install the Anaconda AI package
+  const installResult = await anacondaAiCli.runInstallAiPackageCommand();
+  anacondaAiCli.verifyInstallAiPackageCommand(installResult);
+
+  // Activate the Anaconda AI package environment
+  const activateResult = await anacondaAiCli.runActivateAiPackageEnvCommand();
+  anacondaAiCli.verifyActivateAiPackageEnvCommand(activateResult);
+
+  // Add the Anaconda AI package environment to the AI 
+  const addResult = await anacondaAiCli.runAddAiPackageEnvToSandboxCommand();
+  anacondaAiCli.verifyAddAiPackageEnvToSandboxCommand(addResult);
+
+  // Configure the Anaconda AI package environment to use the AI Catalyst backend
+  const configureResult = await anacondaAiCli.runConfigureAiPackageEnvToUseSandboxCommand();
+  anacondaAiCli.verifyConfigureAiPackageEnvToUseSandboxCommand(configureResult);
+
+  // Verify user is authenticated
+  const authWhoamiResult = await anacondaAiCli.runAuthWhoamiCommand();
+  anacondaAiCli.verifyAuthWhoamiCommand(authWhoamiResult);
 };
